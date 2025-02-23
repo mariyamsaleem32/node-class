da6fc9e0c39325ba9c657c6a633670243b71868e
mariyamsaleem32 <mariyamsaleem87@gmail.com>
2025-02-23 23:50:41 +0500

    use post man

--- index.js
+++ index.js
@@ -1,50 +1,57 @@
 import express from 'express';
 import path from 'path';
 // import cors from 'cors';
+const port = process.env.PORT || 3000
 const app = express()
 // app.use(cors())
 const _dirname = path.resolve();
-const port = process.env.PORT || 3000
 
-// app.get('/',(req, res) =>{
-//     res.send('Hello World!'+ new Date().toLocaleString())
-//     })
+app.get('/',(req, res) =>{
+  res.send('I am using express js!')
+})
 
-    app.get('/:cityName', (req, res) => {
-        let weatherData ={
-          karachi:{
-            city :"karachi",
-            tempInC: 26,
-            humidity: 56,
-            high: 32,
-            low: 23
-          },
-          lahore:{
-            city :"lahore",
-            tempInC: 28,
-            humidity: 56,
-            high: 32,
-            low: 23
-          },
-          islamabad:{
-            city :"islamabad",
-            tempInC: 28,
-            humidity: 56,
-            high: 32,
-            low: 23
-          }
-        }  
-        let userInputCity = req.params.cityName.toLowerCase();
-        let weatherDataToSend = weatherData[userInputCity];
-        if(weatherDataToSend){
-          res.send(weatherDataToSend);
-        }else{
-          res.status(404).send(`Data for ${req.params.cityName} not found`);
-        }
-      })
+app.get('/posts',(req, res) =>{
+    res.send({
+      title:'Hello World!',
+      Date : new Date().toLocaleString()
+    })
+  })
 
-      app.use(express.static(path.join(_dirname,'public'))
-      )
+    // app.get('/:cityName', (req, res) => {
+    //     let weatherData ={
+    //       karachi:{
+    //         city :"karachi",
+    //         tempInC: 26,
+    //         humidity: 56,
+    //         high: 32,
+    //         low: 23
+    //       },
+    //       lahore:{
+    //         city :"lahore",
+    //         tempInC: 28,
+    //         humidity: 56,
+    //         high: 32,
+    //         low: 23
+    //       },
+    //       islamabad:{
+    //         city :"islamabad",
+    //         tempInC: 28,
+    //         humidity: 56,
+    //         high: 32,
+    //         low: 23
+    //       }
+    //     }  
+    //     let userInputCity = req.params.cityName.toLowerCase();
+    //     let weatherDataToSend = weatherData[userInputCity];
+    //     if(weatherDataToSend){
+    //       res.send(weatherDataToSend);
+    //     }else{
+    //       res.status(404).send(`Data for ${req.params.cityName} not found`);
+    //     }
+    //   })
+
+    //   app.use(express.static(path.join(_dirname,'public'))
+    //   )
 
       app.listen(port,() =>{
         console.log(`server is running on ${port}`);
