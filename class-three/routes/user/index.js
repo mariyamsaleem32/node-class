750da910f921138d3c39727f6458811f8e8d8388
mariyamsaleem32 <mariyamsaleem87@gmail.com>
2025-03-09 04:23:48 +0500

    added login function

--- class-three/routes/user/index.js
+++ class-three/routes/user/index.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import {postUser,getUser,deleteUser,updateUser} from './user.js';
+import {postUser,getUser,loginUser,deleteUser,updateUser} from './user.js';
 
 const user_router = express.Router();
 user_router.post('/',postUser);
 user_router.get('/',getUser)
+user_router.post('/login',loginUser)
 user_router.delete('/:id',deleteUser)
 user_router.put('/:id',updateUser)
 
--- class-three/routes/user/user.js
+++ class-three/routes/user/user.js
@@ -1,9 +1,15 @@
+import { message } from 'antd';
 import User from '../../models/user/index.js';
+import bcrypt from 'bcrypt';
 
 // post user
     const postUser = async (req,res) => {
     try{
-        const user = await User.create(req.body);
+        const password = bcrypt.hashSync(req.body.password, 10)
+        const user = await User.create({
+            ...req.body,
+            password
+        });
         const data = user.toObject();
         delete data.password;
         res.status(201).send({
@@ -21,16 +27,27 @@ import User from '../../models/user/index.js';
     const getUser = async(req, res) => {
         try{  
             const users = await User.find();
-            res.status(200).send({
-            status: 200,
-            users
-            });
+            res.status(200).send({status: 200,user});
         }catch(err){
-            res.status(400).send({
-                status: 400,
-                err
-            })
+            res.status(400).send({status: 400,err})
         }};
+
+    // login user
+    const loginUser = async(req, res) => {
+    try{  
+        const {email,password} = req.body;
+        const user = await User.findOne({email});
+        if(user){
+        const checkPassword = bcrypt.compareSync(password,user.password);
+        if(checkPassword){
+        res.status(200).send({status: 200,message:'user login successfully',user});
+        }else{
+        res.status(401).send({status: 401,message:'incorrect password'})}
+        }else{
+        res.status(400).send({status: 400,message:'user not found'})}
+    }catch(err){
+    res.status(500).send({status: 500, err})
+    }};
 
     // update user
     const updateUser = async(req, res) => {
@@ -42,7 +59,7 @@ import User from '../../models/user/index.js';
                 message:'user updated succesfully',
                 });
                 } catch (err) {
-                res.status(400).send({
+                res.status(500).send({
                     status: 400,
                     err  
                 })
@@ -64,4 +81,4 @@ const deleteUser = async(req, res) => {
     })
 }};
 
-export {postUser,getUser,deleteUser,updateUser};
+export {postUser,getUser,loginUser,deleteUser,updateUser};
